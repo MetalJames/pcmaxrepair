b0f769efd8f17a089420aa1b7eec53d1c4b047ef
Volodymyr <volodyaruzhak@gmail.com>
2023-03-14 09:35:42 -0400

    add two more provided services

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,6 +3,8 @@ import menu from './menu.svg';
 import iphonerepair from './Max_Repair_web-05.jpg';
 import smartphonerepair from './pexels-noah-erickson-404280.jpg';
 import macbookrepair from './macbookrepair.png';
+import laptoprepair from './Max_Repair_web-10.jpg';
+import ipadtabletrepair from './ipadtabletrepair.png';
 
 export {
     close,
@@ -10,4 +12,6 @@ export {
     iphonerepair,
     smartphonerepair,
     macbookrepair,
+    laptoprepair,
+    ipadtabletrepair,
 };
--- src/constants/repairs.tsx
+++ src/constants/repairs.tsx
@@ -1,4 +1,4 @@
-import { iphonerepair, smartphonerepair, macbookrepair } from "../assets";
+import { iphonerepair, smartphonerepair, ipadtabletrepair, macbookrepair, laptoprepair } from "../assets";
 
 export const repairs = [
     {
@@ -21,11 +21,11 @@ export const repairs = [
             },
             {
                 id: 4,
-                repair: "camera malfunctioning",
+                repair: "Camera malfunctioning",
             },
             {
                 id: 5,
-                repair: "power/volume button",
+                repair: "Power/volume button",
             },
             {
                 id: 6,
@@ -33,7 +33,7 @@ export const repairs = [
             },
             {
                 id: 7,
-                repair: "no power-related issues (motherboard repair)",
+                repair: "No power-related issues (motherboard repair)",
             },
         ],
         inquiry: [
@@ -59,7 +59,7 @@ export const repairs = [
             },
             {
                 id: 2,
-                repair: "Charging portcharging port",
+                repair: "Charging port",
             },
             {
                 id: 3,
@@ -67,11 +67,11 @@ export const repairs = [
             },
             {
                 id: 4,
-                repair: "camera malfunctioning",
+                repair: "Camera malfunctioning",
             },
             {
                 id: 5,
-                repair: "power/volume button",
+                repair: "Power/volume button",
             },
             {
                 id: 6,
@@ -79,7 +79,7 @@ export const repairs = [
             },
             {
                 id: 7,
-                repair: "no power-related issues (motherboard repair)",
+                repair: "No power-related issues (motherboard repair)",
             },
         ],
         inquiry: [
@@ -93,15 +93,61 @@ export const repairs = [
         image: smartphonerepair,
     },
     {
-        id: 'macbook_repair',
-        title: "Smartphone Repair",
-        description: "At MAX REPAIR, you can get help with simple upgrades on your mac to a motherboard level repairs.",
-        extradescription: "We are specialized in Samsung, Huawei, Xiaomi, Google, and OnePlus.",
-        listTitle: "We can troubleshoot and FIX:",
+        id: 'ipadtablet_repair',
+        title: "iPad / Tablet Repair",
+        description: "At MAX REPAIR, you can get help with your Apple iPad, Microsoft, or any Tablet in no time",
+        extradescription: "We are specialized in Apple iPad's, Microsoft Surface/Book, Samsung, Huawei, Google.",
+        listTitle: "We can FIX:",
         listofrepairs: [
             {
                 id: 1,
-                repair: "software/driver issues with Mac OS",
+                repair: "Broken screen",
+            },
+            {
+                id: 2,
+                repair: "Charging port",
+            },
+            {
+                id: 3,
+                repair: "Faulty battery",
+            },
+            {
+                id: 4,
+                repair: "Camera malfunctioning",
+            },
+            {
+                id: 5,
+                repair: "Power/volume button",
+            },
+            {
+                id: 6,
+                repair: "liquid damage",
+            },
+            {
+                id: 7,
+                repair: "No power-related issues (motherboard repair)",
+            },
+        ],
+        inquiry: [
+            {
+                id: 1,
+                description: "For any inquiry, don’t hesitate to give us a call or send us an email:",
+                phonenumber: "905 517 9610",
+                email: "mailto:max@pcmaxrepair.ca",
+            }
+        ],
+        image: ipadtabletrepair,
+    },
+    {
+        id: 'macbook_repair',
+        title: "MacBook Repair",
+        description: "At MAX REPAIR, you can get help with simple upgrades on your mac to a motherboard level repairs.",
+        extradescription: "We are specialized in Samsung, Huawei, Xiaomi, Google, and OnePlus.",
+        listTitle: "We can troubleshoot and FIX in any MAC:",
+        listofrepairs: [
+            {
+                id: 1,
+                repair: "Software/driver issues with Mac OS",
             },
             {
                 id: 2,
@@ -145,6 +191,60 @@ export const repairs = [
             }
         ],
         image: macbookrepair,
+    },
+    {
+        id: 'laptop_repair',
+        title: "Laptop Repair",
+        description: "At MAX REPAIR, you can get help with simple upgrades on your laptop to a motherboard level repairs.",
+        extradescription: "We are specialized in Asus, Acer, Lenovo, Dell, Toshiba, Chromebook, MSI, and HP.",
+        listTitle: "We can troubleshoot and FIX:",
+        listofrepairs: [
+            {
+                id: 1,
+                repair: "Software/driver issues with Windows OS",
+            },
+            {
+                id: 2,
+                repair: "Laptop HDD/SSD replacement and OS reinstallation and Data transfer",
+            },
+            {
+                id: 3,
+                repair: "Faulty battery",
+            },
+            {
+                id: 4,
+                repair: "Dc/Power jack repair",
+            },
+            {
+                id: 5,
+                repair: "LCD screen replacement/repair",
+            },
+            {
+                id: 6,
+                repair: "liquid damage cleaning and repair.",
+            },
+            {
+                id: 7,
+                repair: "Keyboard and trackpad repair.",
+            },
+            {
+                id: 8,
+                repair: "Virus cleaning", 
+            },
+            {
+                id: 9,
+                repair: "We can recover your lost data or simple transfer it.",
+            },
+        ],
+        inquiry: [
+            {
+                id: 1,
+                description: "For any inquiry, don’t hesitate to give us a call or send us an email:",
+                phonenumber: "905 517 9610",
+                email: "mailto:max@pcmaxrepair.ca",
+            }
+        ],
+        image: laptoprepair,
     },
 ]
 
