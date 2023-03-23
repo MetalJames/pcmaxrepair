bde23a4770f735686546118bf72bc4e836e12b53
Volodymyr <volodyaruzhak@gmail.com>
2023-03-22 21:20:01 -0400

    fixed issue with gaming consoles now its displaying properly move button read more in our services to the right

--- src/components/OurServices.tsx
+++ src/components/OurServices.tsx
@@ -14,14 +14,16 @@ const OurServices = () => {
                                         ease-in-out duration-300
                                         ${index === ourservices.length - 1 ? 'mb-0' : 'mb-8'}`}>
                     <Link key={id} to={`${link}`}>
-                        <div className='flex justify-between flex-col sm:px-10 px-6 sm:py-12 py-8 rounded-[20px] max-w-[370px]'>
-                            <div className='flex text-6xl pb-6 justify-center items-center' >
-                                <Icon className='w-[100px] h-[100px] p-4 bg-[#4e97d1] fill-[white] rounded-[20%]'/>
+                        <div className='flex flex-col justify-between sm:px-10 px-6 sm:py-12 py-8 rounded-[20px] max-w-[370px] h-full'>
+                            <div>
+                                <div className='flex text-6xl pb-6 justify-center items-center' >
+                                    <Icon className='w-[100px] h-[100px] p-4 bg-[#4e97d1] fill-[white] rounded-[20%]'/>
+                                </div>
+                                <h2 className='font-poppins font-semibold text-[18px] leading-[23px] mb-1 text-center'>{title}</h2>
+                                <p className='font-poppins font-light text-[18px] leading-[30.8px] max-w-[470px] mt-5'>{description}</p>
                             </div>
-                            <h2 className='font-poppins font-semibold text-[18px] leading-[23px] mb-1 text-center'>{title}</h2>
-                            <p className='font-poppins font-light text-[18px] leading-[30.8px] max-w-[470px] mt-5'>{description}</p>
-                            <div className='flex items-center pt-4'>
-                                <h4>READ MORE</h4><MdArrowRightAlt />
+                            <div className='flex justify-end items-center pt-4'>
+                                <p className='text-[14px]'>READ MORE</p><MdArrowRightAlt />
                             </div>
                         </div>
                     </Link>
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -17,20 +17,25 @@ const Services = () => {
         <div>
             {repairs.filter(repair => repair.id === params.id).map((repair) => (
                 <div key={repair.id} className='flex flex-col justify-between sm:px-24 px-10 sm:py-4 py-2'>
-                    <h1 className='font-poppins font-semibold xs:text-[48px] text-[40px] xs:leading-[76.8px] leading-[66.8px] w-full text-center sm:pb-10 pb-2'>{repair.title}</h1>
-                    <div className='flex sm:flex-row flex-col-reverse flex-1 justify-between items-center pb-8'>
+                    <h1 className={`font-poppins font-semibold xs:text-[48px] text-[40px] xs:leading-[76.8px] leading-[66.8px] w-full text-center 
+                                    ${ repair.id === 'gamingconsole_repair' && 'sm:pb-4 pb-1'}`}>
+                        {repair.title}
+                    </h1>
+                    <div className={`flex sm:flex-row flex-col-reverse flex-1 justify-between items-center pb-8
+                                    ${ repair.id === 'gamingconsole_repair' && 'hidden'}
+                                    `}>
                         <div className='max-w-[420px]'>
                             <p className='font-poppins font-semibold text-[20px] leading-[32px]'>{repair.description}</p>
                             {repair.extradescription && <p className='font-poppins font-normal text-[16px] leading-[24px] mt-4 flex items-center'>{repair.extradescription}</p>}
                             <p className='font-poppins font-semibold text-[16px] leading-[23px] mb-4 mt-4'>{repair.listTitle}</p>
                             <ul>
-                                {repair.listofrepairs.map((itemrepair) => (
+                                {repair.listofrepairs && repair.listofrepairs.map((itemrepair) => (
                                     <li className='font-poppins font-normal text-[16px] leading-[24px] mb-4 flex items-center' key={itemrepair.id}>
                                         <span><MdCheckCircle style={{ color: 'green' }}/></span>&nbsp;<span>{itemrepair.repair}</span>
                                     </li>
                                 ))}
                             </ul>
-                            {repair.logicboardfailure ? <><p>{repair.logicboardfailure}</p></> : <></>}
+                            {repair.logicboardfailure && <><p>{repair.logicboardfailure}</p></>}
                             {repair.inquiry && repair.inquiry.map(({ id, description, phonenumber, email }) => (
                                 <div key={id}>
                                     <p>{description}</p>
@@ -50,6 +55,48 @@ const Services = () => {
                         </div>
                         <img src={repair.image} alt={repair.title} className='sm:w-[50%] sm:h-[50%] w-[100%] h-[100%] sm:pb-0 pb-6'/>
                     </div>
+                    {repair.listofconsols && repair.listofconsols.map((console) => (
+                        <div key={console.id}>
+                            
+
+
+
+                    <h1 className='font-poppins font-semibold xs:text-[32px] text-[26px] xs:leading-[76.8px] leading-[66.8px] w-full text-center sm:pb-10 pb-2'>{console.title}</h1>
+                    <div className='flex sm:flex-row flex-col-reverse flex-1 justify-between items-center pb-8'>
+                        <div className='max-w-[420px]'>
+                            <p className='font-poppins font-semibold text-[20px] leading-[32px]'>{console.description}</p>
+                            <p className='font-poppins font-semibold text-[16px] leading-[23px] mb-4 mt-4'>{console.listTitle}</p>
+                            <ul>
+                                {console.listofrepairs && console.listofrepairs.map((itemrepair) => (
+                                    <li className='font-poppins font-normal text-[16px] leading-[24px] mb-4 flex items-center' key={itemrepair.id}>
+                                        <span><MdCheckCircle style={{ color: 'green' }}/></span>&nbsp;<span>{itemrepair.repair}</span>
+                                    </li>
+                                ))}
+                            </ul>
+                            {console.inquiry && console.inquiry.map(({ id, description, phonenumber, email }) => (
+                                <div key={id}>
+                                    <p>{description}</p>
+                                    <div className='flex justify-between items-center'>
+                                        <a href='tel:+6199942413'className='flex items-center mt-2'>
+                                            <FaPhoneAlt size='19' className='nav-linker'/>&nbsp;&nbsp;
+                                            <p>{phonenumber}</p>
+                                        </a>
+                                        <a href={email} 
+                                            target='_blank' className='flex items-center mt-2'>
+                                            <MdAlternateEmail size='19' className='nav-linker'/>&nbsp;&nbsp;
+                                            <p>max@pcmaxrepair.ca</p>
+                                        </a>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                        <img src={console.image} alt={repair.title} className='sm:w-[50%] sm:h-[50%] w-[100%] h-[100%] sm:pb-0 pb-6'/>
+                    </div>
+
+
+
+                        </div>
+                    ))}
                 </div>
             ))}
         </div>
