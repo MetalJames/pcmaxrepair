8ec3b9b292b720f6403fc038ffd4ef933649caab
Volodymyr <volodyaruzhak@gmail.com>
2023-03-24 19:42:10 -0400

    cleared from unused code

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -7,11 +7,9 @@ const WhyChooseUs = () => {
             <h2 className='font-poppins font-semibold xs:text-[48px] text-[40px] xs:leading-[76.8px] leading-[66.8px] w-full text-center mb-4'>Why Choose MaxRepair</h2>
             <div className='flex flex-wrap flex-row justify-center w-full'>
                 {whychooseus.map(({ id, title, description, logo: Icon}) => 
-                // <div key={id} className='bg-white text-black sm:w-[20%] w-[100%] rounded-[20px] sm:m-4 m-0 sm:mb-4 mb-4 flex flex-col items-center'>
                 <div key={id} className='text-black sm:w-[20%] w-[100%] rounded-[20px] sm:m-4 m-0 sm:mb-4 mb-4 flex flex-col items-center'>
                     <div className='flex justify-start flex-col p-2 rounded-[20px] w-full'>
                         <div className='flex items-center justify-center sm:ml-0 -ml-6'>
-                            {/* <Icon className='fill-[#4e97d1] text-4xl'/> */}
                             <Icon className='fill-[#4e9] text-4xl'/>
                             <h2 className='font-poppins font-semibold text-[18px] leading-[23px] text-center'>{title}</h2>
                         </div>
