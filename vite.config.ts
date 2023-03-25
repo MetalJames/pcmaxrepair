aae3278424983cdd2b48c66d4c60e537da17d649
Volodymyr <volodyaruzhak@gmail.com>
2023-03-25 13:16:24 -0400

    remove it

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    // use: [
-    //   {
-    //     loader: 'markdown-loader',
-    //     options: {
-    //       basename: process.env.NODE_ENV === "production" ? "/react-router" : undefined
-    //     }
-    // ]
 })
